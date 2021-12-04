ed35b8334c112306a54d5a0a7444d1190aa2b4d1
KingVanti <kingvanti650@gmail.com>
2021-12-04 09:20:14 +0100

    Add helpers for building optionals and results

---
+++ Util/ResultBuilder.fs
@@ -0,0 +1,15 @@
+﻿[<AutoOpen>]
+module AdventOfCode21.ResultBuilder
+
+type ResultBuilder() =
+
+    member this.Bind(x, f) =
+        match x with
+        | Ok ok -> f ok
+        | Error error -> Error error
+
+    member this.Return(x) = Ok x
+
+    member this.ReturnFrom(x) = x
+
+let res = ResultBuilder()
